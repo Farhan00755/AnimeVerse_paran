a13e7c2539ca4453bc6ee6db0fff649e32cf0d97
Muhammad Farhan Prasetyo <tyoprasetyo2007@gmail.com>
2026-09-23 10:28:10 +0700

    syle: sign in screen

---
+++ lib/widgets/gradient_background.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class GradientBackground extends StatelessWidget {
+  final Widget child;
+  const GradientBackground({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color(0xFF333465),
+            Color(0xFF0b395e)
+          ],
+          begin: Alignment.topLeft,
+          end : Alignment.bottomLeft  
+        )
+      ),
+      child: child,
+    );
+  }
+}
