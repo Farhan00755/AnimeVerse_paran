2a9c21c66b46170f481998bef14c758eb5915af5
Muhammad Farhan Prasetyo <tyoprasetyo2007@gmail.com>
2026-09-25 10:07:34 +0700

    update

--- lib/widgets/gradient_background.dart
+++ lib/widgets/gradient_background.dart
@@ -1,28 +1,24 @@
 import 'package:flutter/material.dart';
 
 class GradientBackground extends StatelessWidget {
-  // Widget ini membutuhkan widget lain untuk ditampilkan di atasnya (child)
   const GradientBackground({super.key, required this.child});
 
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    // Kita gunakan Container sebagai dasarnya
     return Container(
-      // Properti decoration untuk styling visual
       decoration: const BoxDecoration(
-        // Definisikan gradien linear
         gradient: LinearGradient(
           colors: [
-            Color(0xFF333465), // Warna awal
-            Color(0xFF0b395e), // Warna akhir
+            Color(0xFF333465),
+            Color(0xFF0b395e),
           ],
-          begin: Alignment.topLeft, // Arah mulai gradien
-          end: Alignment.bottomRight, // Arah akhir gradien
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
         ),
       ),
-      // Tampilkan widget child di atas Container yang sudah bergradien
+      
       child: child,
     );
   }
